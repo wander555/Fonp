3a66b36a5f06072ac5edc37e86a4736b6f1536b8
wander555 <larryzhangqiang@126.com>
2018-03-13 22:44:44 +0800

    init

--- app/src/main/java/com/larryzhang/fonp/MainActivity.java
+++ app/src/main/java/com/larryzhang/fonp/MainActivity.java
@@ -1,21 +1,170 @@
 package com.larryzhang.fonp;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
 
-import com.larryzhang.fonp.fragment.MainFragment;
+import com.larryzhang.fonp.fragment.Fragment1;
 
-import me.yokeyword.fragmentation.SupportActivity;
+import net.lucode.hackware.magicindicator.FragmentContainerHelper;
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.buildins.UIUtil;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ClipPagerTitleView;
 
-public class MainActivity extends SupportActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import me.yokeyword.fragmentation.ExtraTransaction;
+import me.yokeyword.fragmentation.ISupportActivity;
+import me.yokeyword.fragmentation.SupportActivityDelegate;
+import me.yokeyword.fragmentation.anim.FragmentAnimator;
+
+public class MainActivity extends AppCompatActivity implements ISupportActivity {
+
+    private static final String[] CHANNELS = new String[]{"New", "Top", "Love"};
+    @Bind(R.id.magic_indicator1)
+    MagicIndicator magicIndicator;
+    @Bind(R.id.fragment_container)
+    FrameLayout fragmentContainer;
+
+    private List<Fragment> mFragments = new ArrayList<Fragment>();
+    private FragmentContainerHelper mFragmentContainerHelper = new FragmentContainerHelper();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        if (findFragment(MainFragment.class) == null) {
-            loadRootFragment(R.id.fl_container, MainFragment.newInstance());  //load root Fragment
+        initFragments();
+        initMagicIndicator1();
+
+        mFragmentContainerHelper.handlePageSelected(0, false);
+        switchPages(0);
+    }
+
+    private void switchPages(int index) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment fragment;
+        for (int i = 0, j = mFragments.size(); i < j; i++) {
+            if (i == index) {
+                continue;
+            }
+            fragment = mFragments.get(i);
+            if (fragment.isAdded()) {
+                fragmentTransaction.hide(fragment);
+            }
+        }
+        fragment = mFragments.get(index);
+        if (fragment.isAdded()) {
+            fragmentTransaction.show(fragment);
+        } else {
+            fragmentTransaction.add(R.id.fragment_container, fragment);
+        }
+        fragmentTransaction.commitAllowingStateLoss();
+    }
+
+
+    //初始化fragment，有多少加多少
+    private void initFragments() {
+        for (int i = 0; i < CHANNELS.length; i++) {
+            Fragment1 testFragment = new Fragment1();
+            Bundle bundle = new Bundle();
+//            bundle.putString(Fragment1.EXTRA_TEXT, CHANNELS[i]);
+            testFragment.setArguments(bundle);
+            mFragments.add(testFragment);
         }
     }
 
+
+    private void initMagicIndicator1() {
+        magicIndicator.setBackgroundResource(R.drawable.round_indicator_bg);
+        CommonNavigator commonNavigator = new CommonNavigator(this);
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+            @Override
+            public int getCount() {
+                return CHANNELS.length;
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, final int index) {
+                ClipPagerTitleView clipPagerTitleView = new ClipPagerTitleView(context);
+                clipPagerTitleView.setText(CHANNELS[index]);
+                clipPagerTitleView.setTextColor(Color.parseColor("#e94220"));
+                clipPagerTitleView.setClipColor(Color.WHITE);
+                clipPagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mFragmentContainerHelper.handlePageSelected(index);
+                        switchPages(index);
+                    }
+                });
+                return clipPagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                LinePagerIndicator indicator = new LinePagerIndicator(context);
+                float navigatorHeight = 45;
+                float borderWidth = UIUtil.dip2px(context, 1);
+                float lineHeight = navigatorHeight - 2 * borderWidth;
+                indicator.setLineHeight(lineHeight);
+                indicator.setRoundRadius(lineHeight / 2);
+                indicator.setYOffset(borderWidth);
+                indicator.setColors(Color.parseColor("#bc2a2a"));
+                return indicator;
+            }
+        });
+        magicIndicator.setNavigator(commonNavigator);
+        mFragmentContainerHelper.attachMagicIndicator(magicIndicator);
+    }
+
+    @Override
+    public SupportActivityDelegate getSupportDelegate() {
+        return null;
+    }
+
+    @Override
+    public ExtraTransaction extraTransaction() {
+        return null;
+    }
+
+    @Override
+    public FragmentAnimator getFragmentAnimator() {
+        return null;
+    }
+
+    @Override
+    public void setFragmentAnimator(FragmentAnimator fragmentAnimator) {
+
+    }
+
+    @Override
+    public FragmentAnimator onCreateFragmentAnimator() {
+        return null;
+    }
+
+    @Override
+    public void post(Runnable runnable) {
+
+    }
+
+    @Override
+    public void onBackPressedSupport() {
+
+    }
 }
--- app/src/main/java/com/larryzhang/fonp/fragment/Fragment1.java
+++ app/src/main/java/com/larryzhang/fonp/fragment/Fragment1.java
@@ -2,6 +2,7 @@ package com.larryzhang.fonp.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,34 +11,25 @@ import com.larryzhang.fonp.R;
 
 import me.yokeyword.fragmentation.SupportFragment;
 
-/**
- * File description.
- *
- * @author zhangqiang
- * @date 2018/3/12
+/**最新图片
+ * Created by Administrator on 2018/3/13.
  */
 
-public class MainFragment extends SupportFragment {
+public class Fragment1 extends Fragment {
 
-    public static MainFragment newInstance() {
-
+    public static Fragment1 newInstance() {
         Bundle args = new Bundle();
-
-        MainFragment fragment = new MainFragment();
+        Fragment1 fragment = new Fragment1();
         fragment.setArguments(args);
         return fragment;
     }
 
-
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.wechat_fragment_main, container, false);
-        initView(view);
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+//        initView(view);
         return view;
-    }
-
-    private void initView(View view) {
-
     }
 
 
