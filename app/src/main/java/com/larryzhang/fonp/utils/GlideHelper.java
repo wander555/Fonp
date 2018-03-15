932f1382e9393491825cea7c85565337ddc70757
wander555 <larryzhangqiang@126.com>
2018-03-15 15:18:26 +0800

    init

---
+++ app/src/main/java/com/larryzhang/fonp/utils/GlideHelper.java
@@ -0,0 +1,24 @@
+package com.larryzhang.fonp.utils;
+
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+/**
+ * 图片加工具
+ * 2018年3月15日14:11:27
+ */
+public class GlideHelper {
+
+    private GlideHelper() {}
+
+    public static void loadPaintingImage(ImageView image, String url) {
+        Glide.with(image.getContext().getApplicationContext())
+                .load(url)
+                .dontAnimate()
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .into(image);
+    }
+
+}
